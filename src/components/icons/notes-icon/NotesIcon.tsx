801bb42032149ee09abe249f4e00cda473790e50
Eduard Nicolaev <eduard.nicolaev@carasent.com>
2025-04-25 11:01:38 +0000

    Merged in feat/ASMA-5599-integrate-graph-icons-for-revman-system (pull request #28)
    Feat/ASMA-5599 integrate graph icons for revman system
    
    * feat: ASMA-5599 Intergrated graph and notes icons
    
    * v0.0.37

---
+++ src/components/icons/notes-icon/NotesIcon.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from 'react'
+
+export function NotesIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' {...props}>
+            <path
+                d='M3 18.6555V16.7375H15.343V18.6555H3ZM3 13.2875V11.3696H21.293V13.2875H3ZM3 7.93207V6H21.293V7.93207H3Z'
+                fill='currentColor'
+            />
+        </svg>
+    )
+}
