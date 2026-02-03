d37710f3137d1840bee0b7e108b7761fe0b57301
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2026-02-03 14:30:05 +0200

    fix: ASMA-6718 Replaced large checkicon to small one

--- src/components/icons/check-icon/CheckIcon.tsx
+++ src/components/icons/check-icon/CheckIcon.tsx
@@ -2,8 +2,8 @@ import type { SVGProps } from 'react'
 
 export function CheckIcon(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}>
-            <path fill='currentColor' d='M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z'></path>
+        <svg xmlns='http://www.w3.org/2000/svg' width='12' height='10' viewBox='0 0 12 10' fill='none' {...props}>
+            <path d='M4 9.4L0 5.4L1.4 4L4 6.6L10.6 0L12 1.4L4 9.4Z' fill='currentColor' />
         </svg>
     )
 }
