4a55ef0c7090a63b6047477acadd7036d666c3e6
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2026-02-03 14:55:15 +0200

    fix: ASMA-6718 Fied viewBox for check icon

--- src/components/icons/check-icon/CheckIcon.tsx
+++ src/components/icons/check-icon/CheckIcon.tsx
@@ -2,8 +2,8 @@ import type { SVGProps } from 'react'
 
 export function CheckIcon(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns='http://www.w3.org/2000/svg' width='12' height='10' viewBox='0 0 12 10' fill='none' {...props}>
-            <path d='M4 9.4L0 5.4L1.4 4L4 6.6L10.6 0L12 1.4L4 9.4Z' fill='currentColor' />
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' {...props}>
+            <path d='M10 16.4L6 12.4L7.4 11L10 13.6L16.6 7L18 8.4L10 16.4Z' fill='currentColor' />
         </svg>
     )
 }
