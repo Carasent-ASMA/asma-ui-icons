f9cc596a209691fc9c20a02ada8e0dee8c8636ba
Bularga Alexandru <bularga.alexandru@carasent.com>
2026-02-26 14:45:30 +0000

    Merged in fix/ASMA-6488-replace-check-icon (pull request #54)
    Fix/ASMA-6488 replace check icon
    
    * fix: Replaced check icon
    
    * v0.0.77

--- src/components/icons/check-icon/CheckIcon.tsx
+++ src/components/icons/check-icon/CheckIcon.tsx
@@ -2,8 +2,9 @@ import type { SVGProps } from 'react'
 
 export function CheckIcon(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' {...props}>
-            <path d='M10 16.4L6 12.4L7.4 11L10 13.6L16.6 7L18 8.4L10 16.4Z' fill='currentColor' />
+        <svg xmlns='http://www.w3.org/2000/svg' width={24} height={24} viewBox='0 0 24 24' {...props}>
+            <title>Check icon</title>
+            <path fill='currentColor' d='M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z'></path>
         </svg>
     )
 }
