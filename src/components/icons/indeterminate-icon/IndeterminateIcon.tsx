e0f147a01a66a0fc977cc344ab63710b8a27e80b
alexandru-bularga <bularga.alexandru@carasent.com>
2026-04-14 16:50:51 +0300

    ASMA-7120 update all packages for ui families (#4)
    feat: Add ArrowShrinkIcon and IndeterminateIcon components
    
    - Add ArrowShrinkIcon and IndeterminateIcon components and exports
    - Update icons index and useIconsList to include new icons
    
    --force-release

---
+++ src/components/icons/indeterminate-icon/IndeterminateIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { SVGProps } from 'react'
+
+export function IndeterminateIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20' fill='none' {...props}>
+            <path d='M5.83398 10.8332V9.1665H14.1673V10.8332H5.83398Z' fill='currentColor' />
+        </svg>
+    )
+}
